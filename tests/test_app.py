52fcf6887688b61f8f7ee9d797e96dc5acc6bc34
rguevara <rguevara@sourcescrub.com>
2023-08-07 21:06:24 -0600

    fix tests rel_dir

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,11 @@
+import os
 from app.main import create_app
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 
 class TestApp:
-    test_img_url = "./data/python_logo.png"
+    test_img_url = "data/python_logo.png"
 
     @staticmethod
     def init_app():
@@ -17,20 +20,20 @@ class TestApp:
     def test_app_get_edges(self):
         flask_app = self.init_app()
         with flask_app.test_client() as test_client:
-            response = test_client.post("/getEdges", data={"image": self.test_img_url})
+            response = test_client.post("/getEdges", data={"image": os.path.join(dir_path, self.test_img_url)})
             response_data = response.json
             assert isinstance(response_data["img"], list)
 
     def test_app_sharpen(self):
         flask_app = self.init_app()
         with flask_app.test_client() as test_client:
-            response = test_client.post("/sharpen", data={"image": self.test_img_url})
+            response = test_client.post("/sharpen", data={"image": os.path.join(dir_path, self.test_img_url)})
             response_data = response.json
             assert isinstance(response_data["img"], list)
 
     def test_app_blur(self):
         flask_app = self.init_app()
         with flask_app.test_client() as test_client:
-            response = test_client.post("/blur", data={"image": self.test_img_url})
+            response = test_client.post("/blur", data={"image": os.path.join(dir_path, self.test_img_url)})
             response_data = response.json
             assert isinstance(response_data["img"], list)
